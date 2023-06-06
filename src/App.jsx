ed9c164248347dee98446521d74a429b6b190947
saiffardin <saiffardin@gmail.com>
2023-06-06 12:42:10 +0600

    Implement top-banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import NavBar from './Components/NavBar/NavBar'
+import TopBanner from './Components/TopBanner/TopBanner'
 
 function App() {
 
   return (
     <>
       <NavBar/>
+      <TopBanner/>
     </>
   )
 }
--- src/utils/btnStyles.js
+++ src/utils/btnStyles.js
@@ -3,6 +3,9 @@ export const GET_STARTED_BTN = {
   height: '52px',
   background: '#58BF73',
   borderRadius: '10px',
+  color: '#FFFFFF',
+  fontWeight: '600',
+
 };
 
 export const LOGIN_BTN = {
@@ -12,4 +15,33 @@ export const LOGIN_BTN = {
   border: '1px solid #181F1B',
   borderRadius: '10px',
   marginLeft: '24px',
+  fontWeight: '600',
+
 };
+
+export const VIVERRA_BTN = {
+  width: '238px',
+  height: '52px',
+  fontFamily: 'Roboto',
+  fontStyle: 'normal',
+  fontWeight: '600',
+  fontSize: '18px',
+  lineHeight: '21px',
+  textAlign: 'center',
+  border: '1px solid #181F1B',
+  borderRadius: '10px',
+  color: '#000000',
+}
+
+export const DEMO_BTN = {
+  width: '238px',
+  height: '52px',
+  marginLeft: '24px',
+  color: '#FFFFFF',
+  background: '#58BF73',
+  borderRadius: '10px',
+  fontStyle: 'normal',
+  fontWeight: '600',
+  fontSize: '18px',
+  lineHeight: '23px',
+}
